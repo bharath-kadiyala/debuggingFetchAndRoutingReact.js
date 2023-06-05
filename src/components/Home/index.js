b88ad6a61980fcde31876efe07664ea6a365473c
bharath-kadiyala <kadiyalabharath8877@gmail.com>
2023-06-05 15:48:32 +0000

    commit message

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+import BlogList from '../BlogList'
+import UserInfo from '../UserInfo'
+
+const Home = () => (
+  <div className="home-container" data-testid="loader">
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+
+export default Home
